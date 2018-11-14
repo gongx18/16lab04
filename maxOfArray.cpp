fb338e14abd7a38d15604551d332f53c31b6b7b0
Xi Gong <xi@ucsb.edu>
2018-11-13 20:46:39 -0800

    Final?

--- maxOfArray.cpp
+++ maxOfArray.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 
 int maxOfArray(int a[], int size) {
+   if (size < 1) {
+     std::cerr << "ERROR: maxOfArray called with size < 1" << std::endl;
+     exit(1);
+   }
   int max = a[0];
   for(int i=1; i<size; i++){
 	  if(a[i]>max){
